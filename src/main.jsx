8969281116e1c8a7a6682bfc4e8f37a5a9c979d7
shifachaus <chausshifa804@gmail.com>
2024-06-24 15:09:08 +0530

    add type check on props

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import StarRating from "./StarRating.jsx";
 // import './index.css'
 
+function Test() {
+  const [movieRating, setMovieRating] = useState(0);
+
+  return (
+    <div className="">
+      <StarRating color="blue" maxRating={10} onSetRating={setMovieRating} />
+      <p>This movie was rated {movieRating} stars</p>
+    </div>
+  );
+}
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     {/* <App /> */}
-    <StarRating maxRating={5} />
+    <StarRating
+      maxRating={5}
+      size={24}
+      className="test"
+      messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
+      defaultRating={3}
+    />
+    <Test />
   </React.StrictMode>
 );
